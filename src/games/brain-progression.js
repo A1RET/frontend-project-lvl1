1ad1e91bca7874b87db0ca565b92b16db1dd3a64
Airet <thiefon@gmail.com>
2019-07-14 14:57:19 +0500

    Step 8

---
+++ src/games/brain-progression.js
@@ -0,0 +1,36 @@
+import engine, { randomNum } from '..';
+
+const game = () => {
+  const description = 'What number is missing in the progression?\n';
+
+  const qA = () => {
+    let num = randomNum(0, 10);
+    const step = randomNum(1, 10);
+    const index = randomNum(1, 10);
+    let question = (`${num}`);
+    let answer = 0;
+
+    if (index === 1) {
+      question = '.. ';
+      answer = String(num);
+    }
+
+    for (let i = 2; i <= 10; i += 1) {
+      num += step;
+      if (i === index) {
+        question = `${question} ..`;
+        answer = String(num);
+      } else {
+        question = `${question} ${num}`;
+      }
+    }
+
+    return [question, answer];
+  };
+
+  return [description, qA];
+};
+
+export default () => {
+  engine(game());
+};
