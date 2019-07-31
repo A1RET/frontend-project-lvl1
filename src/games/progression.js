5b0c9d45531a3e8be0f65aad89f0b488d2fde53d
Airet <thiefon@gmail.com>
2019-07-31 21:14:57 +0500

    переименовал переменную

--- src/games/progression.js
+++ src/games/progression.js
@@ -6,20 +6,20 @@ const description = 'What number is missing in the progression?';
 const progressionLength = 10;
 
 const makeGameData = () => {
-  const startElement = makeRandomNum(0, 10);
+  const init = makeRandomNum(0, 10);
   const diff = makeRandomNum(1, 10);
   const hiddenElementIndex = makeRandomNum(0, progressionLength - 1);
   let question = '';
 
   for (let i = 0; i < progressionLength; i += 1) {
-    let nextElement = startElement + diff * i;
+    let nextElement = init + diff * i;
     if (i === hiddenElementIndex) {
       nextElement = '..';
     }
     question = `${question} ${nextElement}`.trim();
   }
 
-  const answer = startElement + diff * hiddenElementIndex;
+  const answer = init + diff * hiddenElementIndex;
 
   return [question, answer];
 };
